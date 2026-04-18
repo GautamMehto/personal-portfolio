997a6669b7e79e7175601cb3f00042856909e3c6
Gautam Mehto <1mehetogautam@gmail.com>
2026-04-18 18:17:38 +0530

    Updates

---
+++ assets/AnimatedButton-6X8VcJpa.js
@@ -0,0 +1,41 @@
+import{j as r}from"./index-dikzeXGh.js";function l({label:a,icon:e,path:n,className:t,download:s,external:i}){return r.jsxs("a",{href:n,...s&&{download:!0},...i&&{target:"_blank",rel:"noopener noreferrer"},className:`
+        group relative flex items-center justify-center gap-5
+        px-7 py-3 text-base
+        rounded-full
+        w-fit
+        border border-brand
+        text-brand!
+        bg-dark-bg
+        cursor-pointer overflow-hidden
+        transition-all duration-[600ms] ease-[cubic-bezier(0.23,1,0.32,1)]
+        hover:text-text-primary!
+        active:scale-95
+        active:shadow-[0_0_0_4px_rgb(255,255,255)]
+        font-semibold
+        ${t}
+      `,children:[r.jsx("span",{className:`\r
+          absolute left-[-25%] z-10\r
+          transition-all duration-[800ms] ease-[cubic-bezier(0.23,1,0.32,1)]\r
+          group-hover:left-4\r
+          group-hover:text-text-primary\r
+        `,children:e?r.jsx(e,{className:"size-5"}):null}),r.jsx("span",{className:`\r
+          relative z-10\r
+          translate-x-[-12px]\r
+          transition-all duration-[800ms] ease-[cubic-bezier(0.23,1,0.32,1)]\r
+          group-hover:translate-x-[12px]\r
+        `,children:a}),r.jsx("span",{className:`\r
+          absolute top-1/2 left-1/2\r
+          h-5 w-5\r
+          -translate-x-1/2 -translate-y-1/2\r
+          rounded-full\r
+          bg-brand\r
+          opacity-0\r
+          transition-all duration-[800ms] ease-[cubic-bezier(0.23,1,0.32,1)]\r
+          group-hover:h-[220px]\r
+          group-hover:w-[220px]\r
+          group-hover:opacity-100\r
+        `}),r.jsx("span",{className:`\r
+          absolute right-4 z-10\r
+          transition-all duration-[800ms] ease-[cubic-bezier(0.23,1,0.32,1)]\r
+          group-hover:right-[-25%]\r
+        `,children:e&&r.jsx(e,{className:"size-5"})})]})}export{l as default};
