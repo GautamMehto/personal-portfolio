e2c2a5670b8dfcfda3bf134641e5f9f827533c32
Gautam Mehto <1mehetogautam@gmail.com>
2026-04-18 19:07:54 +0530

    Fixing URLs Path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { CursorProvider } from './components/Custom/CursorContext.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
 import Loader from './components/common/Loader.jsx'
@@ -12,7 +12,7 @@ import ThemeToggle from './components/common/ThemeToggle.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <HashRouter>
+      <BrowserRouter basename="/personal-portfolio">
         <CursorProvider>
           <GlobalCursor />
           <Loader>
@@ -20,7 +20,7 @@ createRoot(document.getElementById('root')).render(
           </Loader>
           <ThemeToggle />
         </CursorProvider>
-      </HashRouter>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
 )
